9e5596b5d77ecc3f1a77d3db2f0b3e59fae102a4
Matthew Brown <me@matthewbrown.io>
2020-09-29 10:12:45 +0100

    add firestore repository

--- rockimage/main.py
+++ rockimage/main.py
@@ -24,7 +24,7 @@ if config.ENABLE_API:
         result = service.save_image(image)
         return jsonify(result), 200
 
-    @app.route("/images/<uuid:str>")
+    @app.route("/images/<uuid:uuid>")
     def get_image(uuid: str):
         image = service.get_image(uuid)
         return jsonify(image), 200
--- rockimage/repository.py
+++ rockimage/repository.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
+from uuid import UUID
 from dataclasses import dataclass
 from typing import List, Optional
 
 import sqlalchemy as sa
+from google.cloud import firestore
 
 from rockimage import config, db, models
 
@@ -29,7 +31,7 @@ class Repository(ABC):
 
 class SQLRepository(Repository):
     def get_image(self, uuid: str) -> Optional[ImageRecord]:
-        result = db.session.query(models.Image).first()
+        result = db.session.query(models.Image).filter(models.Image.uuid == uuid).first()
         if not result:
             return None
         return ImageRecord(path=result.path, uuid=result.uuid)
@@ -45,4 +47,30 @@ class SQLRepository(Repository):
         return ImageRecord(uuid=uuid, path=path)
 
 
-repository = SQLRepository()
+class FirestoreRepository(Repository):
+    def __init__(self):
+        self.client = firestore.Client()
+
+    def get_image(self, uuid: str) -> Optional[ImageRecord]:
+        result = self.client.collection(u"images").document(uuid).get()
+        if not result.exists:
+            return None
+        return ImageRecord(path=result.get("path"), uuid=result.get("uuid"))
+
+    def get_images(self) -> Optional[ImageRecord]:
+        results = self.client.collection(u"images").stream()
+        return [ImageRecord(path=result.path, uuid=result.uuid) for result in results]
+
+    def save_image(self, uuid: str, path: str) -> ImageRecord:
+        self.client.collection(u"images").document(uuid).set(
+            {
+                "uuid": uuid,
+                "path": path,
+            }
+        )
+        return ImageRecord(uuid=uuid, path=path)
+
+
+impls = {"sql": SQLRepository, "firestore": FirestoreRepository}
+
+repository = impls[config.DATASTORE]()
